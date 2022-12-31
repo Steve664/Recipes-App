f509b28049ea1b56695f08264d84612c43db037b
Steve Kariuki <stevekariuki5@gmail.com>
2022-12-31 07:18:54 +0300

    function to geat meal added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("searchBtn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+searchBtn.addEventListener("click", e => {
+    let input = document.getElementById('input').value
+    getMeal(input)
+})
+
+function getMeal(input) {
+    fetch(url + input)
+        .then(resp => resp.json)
+        .then(data => {
+            let myMeal = data.meals[0]
+            displayMeal(myMeal)
+        })
+}
+
